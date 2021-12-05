4b7e3d1dd96c8bce4e9ad4c12b564cb64a524d36
Sophie Goldensohn <94643892+sophiegoldensohn@users.noreply.github.com>
2021-12-05 18:47:00 +0000

    I defined the base, height, and area as appropriate. Then I wrote a command to print the desired text.

--- part4.py
+++ part4.py
@@ -14,3 +14,7 @@ The area of the triangle is 12.0
 '''
 
 #start writing your code below
+base = int(input("Enter the base: "))
+height = int(input("Enter the height: "))
+area = (base * height)/2
+print ("The area of the triangle is", area)
