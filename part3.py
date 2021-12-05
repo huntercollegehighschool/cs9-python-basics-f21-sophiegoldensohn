f35f0e13b16c8797ba4b0c8bffc6d1c476440f17
Sophie Goldensohn <94643892+sophiegoldensohn@users.noreply.github.com>
2021-12-05 18:08:43 +0000

    tentimes and ninetyninemore were defined.

--- part3.py
+++ part3.py
@@ -6,9 +6,18 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 
 '''
 
-#code starts here
-number1 = input("Enter a number: ")
-number2 = int(input("Enter another number: ")
+#original code starts here
+#number1 = input("Enter a number: ")
+#number2 = int(input("Enter another number: ")
 
-print "The sum of your numbers is", number1 + Number2
-print(Seven times your second number is, 7(number2))
+#print "The sum of your numbers is", number1 + Number2
+#print(Seven times your second number is, 7(number2))
+
+number1 = int(input("Enter a number: "))
+#error 1: missing parentheses, error 2: missing int
+number2 = int(input("Enter another number: "))
+#error 3: missing end parentheses
+print ("The sum of your numbers is", number1 + number2)
+#error 4: missing parentheses, error 5: capitalized N in number2
+print("Seven times your second number is", 7 * number2)
+#error 5: missing quotation marks, error 6: incorrect format for multiplication with 7(number2)
